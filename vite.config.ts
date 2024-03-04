e20b73bb352d2eadba3c88d4ac49bb810050dcd3
Felix Brugger <LeMonkay.VTIT@tutanota.com>
2024-03-04 11:51:15 +0000

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/MonkeyMon-React/",
   plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
