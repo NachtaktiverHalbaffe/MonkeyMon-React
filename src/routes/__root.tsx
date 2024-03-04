741aab5416f33b2d21e1bc0fd8bf6bdc1d1e396a
Felix Brugger <LeMonkay.VTIT@tutanota.com>
2024-03-04 08:45:14 +0000

    Basic setup of tanstack router

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,13 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <body className="h-screen w-screen flex items-center justify-center">
       <Outlet />
+      <ReactQueryDevtools buttonPosition="bottom-right" />
       <TanStackRouterDevtools />
-    </>
+    </body>
   ),
-})
+});
