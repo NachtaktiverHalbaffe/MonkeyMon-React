75a8c0081989f5eee1c9e4fc4e237ed57ff38692
Felix Brugger <LeMonkay.VTIT@tutanota.com>
2024-03-06 14:30:25 +0000

    Added battlesprite

---
+++ src/components/battle-sprite.tsx
@@ -0,0 +1,68 @@
+import { TrimmedImage } from "@/components/ui/trimmed-image";
+import { cn } from "@/lib/utils";
+import React, { useState } from "react";
+
+interface BattleSpriteProps {
+  src: string;
+  posX: number;
+  posY: number;
+  className?: string;
+  isExternal?: boolean;
+}
+
+export const BattleSprite = (props: BattleSpriteProps) => {
+  const [position, setPosition] = useState<{ x: number; y: number }>({
+    x: props.posX,
+    y: props.posY,
+  });
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [startPosition, setStartPosition] = useState<{ x: number; y: number }>({
+    x: props.posX,
+    y: props.posY,
+  });
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+    setStartPosition({
+      x: event.clientX - position.x,
+      y: event.clientY - position.y,
+    });
+    console.log(position);
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (isDragging) {
+      setPosition({
+        x: event.clientX - startPosition.x,
+        y: event.clientY - startPosition.y,
+      });
+    }
+  };
+
+  return (
+    <div
+      className="relative"
+      style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseDown={handleMouseDown}
+    >
+      <div
+        className={cn(
+          "absolute bottom-0 left-0 w-96 h-48 bg-[url('src/assets/battle_tile.png')] bg-bottom bg-contain bg-no-repeat",
+          props.className
+        )}
+      />
+      <div
+        className="absolute bottom-0 left-0 w-auto h-fit translate-x-36 -translate-y-16 scale-[2.0] flex items-center justify-center"
+        onMouseDown={handleMouseDown}
+      >
+        <TrimmedImage src={props.src} trimWidth={true} trimHeight={true} />
+      </div>
+    </div>
+  );
+};
---
+++ src/components/ui/trimmed-image.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { LoadingSpinner } from "./loading-spinner";
+
+interface TrimmedNetworkImageProps {
+  src: string;
+  trimHeight?: boolean;
+  trimWidth?: boolean;
+  className?: string;
+}
+
+export const TrimmedImage = (props: TrimmedNetworkImageProps) => {
+  const [trimmedImageUrl, setTrimmedImageUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    console.log("use effect hook");
+    const loadImage = async (
+      url: string,
+      trimHeight: boolean = true,
+      trimWidth: boolean = true
+    ) => {
+      try {
+        const response = await fetch(url);
+        const blob = await response.blob();
+
+        const img = new Image();
+        img.src = URL.createObjectURL(blob);
+        img.crossOrigin = "Anonymous";
+
+        img.onload = () => {
+          const canvas = document.createElement("canvas");
+          const context = canvas.getContext("2d");
+          if (context) {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(img, 0, 0);
+
+            const imageData = context.getImageData(
+              0,
+              0,
+              canvas.width,
+              canvas.height
+            );
+            const { data } = imageData;
+
+            let minX = canvas.width;
+            let minY = canvas.height;
+            let maxX = 0;
+            let maxY = 0;
+
+            for (let y = 0; y < canvas.height; y++) {
+              for (let x = 0; x < canvas.width; x++) {
+                const alpha = data[(y * canvas.width + x) * 4 + 3];
+
+                if (alpha > 0) {
+                  minY = Math.min(minY, y);
+                  maxY = Math.max(maxY, y);
+                  minX = Math.min(minX, x);
+                  maxX = Math.max(maxX, x);
+                }
+              }
+            }
+
+            const trimmedWidth = trimWidth ? maxX - minX + 1 : canvas.width;
+            const trimmedHeight = trimHeight ? maxY - minY + 1 : canvas.height;
+            const trimmedCanvas = document.createElement("canvas");
+            const trimmedContext = trimmedCanvas.getContext("2d");
+
+            if (trimmedContext) {
+              trimmedCanvas.width = trimmedWidth;
+              trimmedCanvas.height = trimmedHeight;
+              trimmedContext.drawImage(
+                img,
+                minX,
+                minY,
+                trimmedWidth,
+                trimmedHeight,
+                0,
+                0,
+                trimmedWidth,
+                trimmedHeight
+              );
+              const trimmedImageData = trimmedCanvas.toDataURL();
+              setTrimmedImageUrl(trimmedImageData);
+            }
+          }
+        };
+      } catch (error) {
+        console.error("Error lading image:", error);
+        toast("Couldnt load image", {
+          description: "",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        });
+      }
+    };
+    loadImage(props.src, props.trimHeight, props.trimWidth);
+  }, [props.src, props.trimHeight, props.trimWidth]);
+
+  return trimmedImageUrl ? (
+    <img className={props.className} src={trimmedImageUrl} draggable={false} />
+  ) : (
+    <LoadingSpinner />
+  );
+};
